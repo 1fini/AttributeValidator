e7a4f18a1a7f0b18a1ce3decd4188e50559b47b2
JD <hinomura.yo@gmail.com>
2018-08-28 20:58:55 +0200

    Compliance with puresharp 4.0.0

--- ParameterValidator/Initialisation.cs
+++ ParameterValidator/Initialisation.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net.Mail;
 using System.Reflection;
 using System.ComponentModel.DataAnnotations;
+using Puresharp.Legacy;
 
 namespace ParameterValidator
 {
@@ -13,7 +14,7 @@ namespace ParameterValidator
         [Startup]
         static public void AutoInscription()
         {
-            foreach(var type in System.Reflection.Assembly.GetAssembly(typeof(EmailValidator)).DefinedTypes)
+            foreach(var type in System.Reflection.Assembly.GetAssembly(typeof(Initialisation)).DefinedTypes)
             {
                 if (type.BaseType == typeof(Aspect))
                 {
@@ -30,45 +31,70 @@ namespace ParameterValidator
     [AttributeUsage(AttributeTargets.Method)]
     public class Service : Attribute
     {
-
-    }
-
-    public class EmailValidator : IValidator
-    {
-        public void Validate<T>(ParameterInfo parameter, T value)
-        {
-            try
-            {
-                new MailAddress(value.ToString());
-            }
-            catch(Exception ex)
-            {
-                throw new ArgumentException(ex.Message);
-            }
-        }
     }
 
     public class EmailValidationAspect : Aspect
     {
-        public override IEnumerable<Func<IAdvice>> Advise(MethodBase method)
+        public override IEnumerable<Advisor> Manage(MethodBase method)
         {
-            yield return Validation<EmailAddressAttribute>.With<EmailValidator>(method);
-        }
-    }
-
-    public class CreditCardValidator : IValidator
-    {
-        public void Validate<T>(ParameterInfo parameter, T value)
-        {
-            new CreditCardAttribute().IsValid(value);
+            yield return Advice
+                .For(method)
+                .Parameter<EmailAddressAttribute>()
+                .Validate((_Parameter, _Attribute, _Value) =>
+                {
+                    if (_Value == null) { throw new ArgumentNullException(_Parameter.Name); }
+                    try { new MailAddress(_Value.ToString()); }
+                    catch (Exception exception) { throw new ArgumentException(_Parameter.Name, exception); }
+                });
         }
     }
 
     public class CreditCardValidationAspect : Aspect
     {
-        public override IEnumerable<Func<IAdvice>> Advise(MethodBase method)
+        public override IEnumerable<Advisor> Manage(MethodBase method)
         {
-            yield return Validation<CreditCardAttribute>.With<CreditCardValidator>(method);
+            yield return Advice
+                .For(method)
+                .Parameter<EmailAddressAttribute>()
+                .Validate((_Parameter, _Attribute, _Value) =>
+                {
+                    if (_Value == null) { throw new ArgumentNullException(_Parameter.Name); }
+                    try { new CreditCardAttribute().IsValid(_Value); }
+                    catch (Exception exception) { throw new ArgumentException(_Parameter.Name, exception); }
+                });
+        }
+    }
+}
+
+namespace Puresharp.Legacy
+{
+    static public class Validation
+    {
+        static public Advisor Validate<T>(this Advisor.Parameter<T> @this, Action<ParameterInfo, T, string> validate)
+            where T : Attribute
+        {
+            return @this.Visit((_Parameter, _Attribute) => new Validator<T>(_Parameter, _Attribute, validate));
+        }
+    }
+
+    public class Validator<T> : IVisitor
+        where T : Attribute
+    {
+        private ParameterInfo m_Parameter;
+        private T m_Attribute;
+        private Action<ParameterInfo, T, string> m_Validate;
+
+        public Validator(ParameterInfo parameter, T attribute, Action<ParameterInfo, T, string> validate)
+        {
+            this.m_Parameter = parameter;
+            this.m_Attribute = attribute;
+            this.m_Validate = validate;
+        }
+
+        void IVisitor.Visit<T>(Func<T> value)
+        {
+            var _value = value();
+            this.m_Validate(this.m_Parameter, this.m_Attribute, _value == null ? null : _value.ToString());
         }
     }
 }
