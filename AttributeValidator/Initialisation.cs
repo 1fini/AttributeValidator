c8796b0fc2dfe3a7c4458c2e6f1f57c617bce0d4
1fini <hinomura.yo@gmail.com>
2018-09-25 23:42:25 +0200

    Added support for phone number and Url attributes

--- AttributeValidator/Initialisation.cs
+++ AttributeValidator/Initialisation.cs
@@ -67,6 +67,38 @@ namespace AttributeValidator
                 });
         }
     }
+
+    internal class PhoneNumberValidationAspect : Aspect
+    {
+        public override IEnumerable<Advisor> Manage(MethodBase method)
+        {
+            yield return Advice
+                .For(method)
+                .Parameter<PhoneAttribute>()
+                .Validate((_Parameter, _Attribute, _Value) =>
+                {
+                    if (_Value == null) { throw new ArgumentNullException(_Parameter.Name); }
+                    try { if (!(new PhoneAttribute().IsValid(_Value))) { throw new Exception(string.Format("Phone number {0} not valid.",_Value)); }; }
+                    catch (Exception exception) { throw new ArgumentException(_Parameter.Name, exception); }
+                });
+        }
+    }
+
+    internal class UrlValidationAspect : Aspect
+    {
+        public override IEnumerable<Advisor> Manage(MethodBase method)
+        {
+            yield return Advice
+                .For(method)
+                .Parameter<UrlAttribute>()
+                .Validate((_Parameter, _Attribute, _Value) =>
+                {
+                    if (_Value == null) { throw new ArgumentNullException(_Parameter.Name); }
+                    try { if (!(new UrlAttribute().IsValid(_Value))) { throw new Exception(string.Format("Phone number {0} not valid.", _Value)); }; }
+                    catch (Exception exception) { throw new ArgumentException(_Parameter.Name, exception); }
+                });
+        }
+    }
 }
 
 namespace Puresharp.Legacy
